c0e6a94f31fe9f6bee0ae9fa338ff3ce0cc9a4a6
ChiNguyen-dev <110326321+ChiNguyen-dev@users.noreply.github.com>
2023-02-10 15:41:26 +0700

    Initial commit

---
+++ code/mdb-angular-ui-kit/popconfirm/popconfirm-ref.ts
@@ -0,0 +1,44 @@
+import { OverlayRef } from '@angular/cdk/overlay';
+import { first, Observable, Subject } from 'rxjs';
+import { MdbPopconfirmContainerComponent } from './popconfirm-container.component';
+
+export class MdbPopconfirmRef<T> {
+  constructor(
+    public overlayRef: OverlayRef,
+    private _popconfirmContainerRef: MdbPopconfirmContainerComponent
+  ) {}
+
+  private readonly onClose$: Subject<any> = new Subject();
+  private readonly onConfirm$: Subject<any> = new Subject();
+
+  readonly onClose: Observable<any> = this.onClose$.asObservable();
+  readonly onConfirm: Observable<any> = this.onConfirm$.asObservable();
+
+  close(message?: any): void {
+    this._popconfirmContainerRef.startCloseAnimation();
+
+    this._popconfirmContainerRef._hidden.pipe(first()).subscribe(() => {
+      this.onClose$.next(message);
+      this.onClose$.complete();
+
+      this.overlayRef.detach();
+      this.overlayRef.dispose();
+    });
+  }
+
+  confirm(message?: any): void {
+    this._popconfirmContainerRef.startCloseAnimation();
+
+    this._popconfirmContainerRef._hidden.pipe(first()).subscribe(() => {
+      this.onConfirm$.next(message);
+      this.onConfirm$.complete();
+
+      this.overlayRef.detach();
+      this.overlayRef.dispose();
+    });
+  }
+
+  getPosition(): DOMRect {
+    return this.overlayRef.overlayElement.getBoundingClientRect();
+  }
+}
