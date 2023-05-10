143d2d8a391ae8c7891903e2fb7b4dd14d83d8f9
NhuanNHP <104629850+NhuanNHP@users.noreply.github.com>
2023-05-10 21:35:04 +0700

    update color for product item

--- src/app/components/client/shop/product-list/product-item/product-item.component.ts
+++ src/app/components/client/shop/product-list/product-item/product-item.component.ts
@@ -1,13 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IProduct} from "../../../../../model/IProduct";
+import {IProductVariant} from "../../../../../model/IProductVariant";
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
-export class ProductItemComponent {
-@Input() product !: IProduct;
+export class ProductItemComponent implements OnInit {
+  @Input() product !: IProduct;
+  variantsColors: IProductVariant[] = []
 
+  ngOnInit(): void {
+    this.getVariantColor();
+  }
 
+  getVariantColor() {
+    let colors = new Set();
+    this.product.variants.map(value => {
+      colors.add(value.color);
+    })
+    colors.forEach(value => {
+      let variantColor = this.product.variants.find(variant => variant.color == value)
+      if (variantColor) {
+        this.variantsColors.push(variantColor);
+      }
+    })
+
+  }
 }
